7e41d9f2ca0df8aad20d7d1fc7d8d6e12f560462
이현규 <gusrb2776@naver.com>
2024-07-19 10:35:16 +0900

    [✨ feat ] 백엔드 프로젝트 생성
    백엔드 프로젝트 생성

---
+++ backend/src/test/java/e106/emissary_backend/EmissaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package e106.emissary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
