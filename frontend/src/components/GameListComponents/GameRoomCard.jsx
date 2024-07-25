ad1a84bc0e3a4c5bc6b360c2117085b3850bb762
김송희 <kkbuh2@gmail.com>
2024-07-25 11:11:10 +0900

    [feat] GLFooter 생성 및 GLMain 수정

--- frontend/src/components/GameListComponents/GameRoomCard.jsx
+++ frontend/src/components/GameListComponents/GameRoomCard.jsx
@@ -1,16 +1,19 @@
-import styles from "./GameRoomCard.module.css"
+import { Link } from "react-router-dom";
+import styles from "./GameRoomCard.module.css";
 
-const GameRoomCard = () => {
-
+const GameRoomCard = ({ title, leader, progress, isInProgress }) => {
     return (
-        <div className={styles.cardContainer}>
-            <div>
-                <p>방 제목</p>
-                <p>대장 동지 : 강진</p>
-            </div>
-            <p>7/8</p>
+        <div>
+            <Link to="/game-room" className={`${styles.card} ${isInProgress ? styles.inProgress : styles.notStarted}`}>
+                <div>
+                    <p className={styles.title}>{title}</p>
+                    <p className={styles.leader}>대장 동지 : {leader}</p>
+                </div>
+                <p className={styles.progress}>{progress}/8</p>
+            </Link>
         </div>
     );
 };
 
-export default GameRoomCard
+export default GameRoomCard;
+
