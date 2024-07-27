6c0b4e1c4af9ecd4fd5297dbb38bc39fb1b334dc
김강진 <kkj1250@gmail.com>
2024-07-27 16:44:56 +0900

    Revert "Merge branch '262-psb' into 'master'"
    This reverts merge request !14

--- backend/src/main/java/e106/emissary_backend/global/error/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package e106.emissary_backend.global.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    String name();
-    HttpStatus getHttpStatus();
-    String getMessage();
-}
