aa5a0376a64d5fd1396cabe7255d62b4a8601a52
김강진 <kkj1250@gmail.com>
2024-08-01 17:10:28 +0900

    [fix] resolve redis config
    Former-commit-id: 40624c1d5d682f38007fb8da66cdb21d93dc4ca4

--- backend/src/main/java/e106/emissary_backend/global/config/RedisConfig.java
+++ backend/src/main/java/e106/emissary_backend/global/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package e106.emissary_backend.global.config;
 
+import e106.emissary_backend.domain.game.entity.Game;
+import e106.emissary_backend.domain.game.service.subscriber.DaySubscriber;
+import e106.emissary_backend.domain.game.service.subscriber.EndVoteSubscriber;
+import e106.emissary_backend.domain.game.service.subscriber.StartConfirmSubscriber;
+import e106.emissary_backend.domain.game.service.subscriber.StartVoteSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +14,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.HashMap;
@@ -24,8 +32,8 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<Long, Game> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Long, Game> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new GenericToStringSerializer<>(Long.class));
         redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Long.class));
@@ -33,4 +41,90 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    @Bean
+    public RedisTemplate<String, HashMap<Long, Integer>> voteRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, HashMap<Long, Integer>> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(HashMap.class));
+        return template;
+    }
+
+    @Bean
+    public RedisKeyValueTemplate redisKeyValueTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Long, Game> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.afterPropertiesSet();
+        return new RedisKeyValueTemplate(new RedisKeyValueAdapter(redisTemplate), new RedisMappingContext());
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory,
+                                                                       MessageListenerAdapter dayListenerAdapter,
+                                                                       ChannelTopic dayTopic,
+                                                                       MessageListenerAdapter startVoteAdapter, ChannelTopic startVoteTopic,
+                                                                       MessageListenerAdapter endVoteAdapter, ChannelTopic endVoteTopic,
+                                                                       MessageListenerAdapter startConfirmAdapter, ChannelTopic startConfirmTopic,
+                                                                       MessageListenerAdapter endConfirmAdapter, ChannelTopic endConfirmTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        // subscriber, topic
+        container.addMessageListener(dayListenerAdapter, dayTopic);
+        container.addMessageListener(startVoteAdapter, startVoteTopic);
+        container.addMessageListener(endVoteAdapter, endVoteTopic);
+        container.addMessageListener(startConfirmAdapter, startConfirmTopic);
+        container.addMessageListener(endConfirmAdapter, endConfirmTopic);
+
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter dayListenerAdapter(DaySubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
+
+    @Bean
+    public ChannelTopic dayTopic() {
+        return new ChannelTopic("DAY");
+    }
+
+    @Bean
+    public MessageListenerAdapter startVoteAdapter(StartVoteSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
+
+    @Bean
+    public ChannelTopic startVoteTopic() {
+        return new ChannelTopic("START_VOTE");
+    }
+
+    @Bean
+    public MessageListenerAdapter endVoteAdapter(EndVoteSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
+
+    @Bean
+    public ChannelTopic endVoteTopic() {
+        return new ChannelTopic("END_VOTE");
+    }
+
+    @Bean
+    public MessageListenerAdapter startConfirmAdapter(StartConfirmSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
+
+    @Bean
+    public ChannelTopic startConfirmTopic() {
+        return new ChannelTopic("START_CONFIRM");
+    }
+
+    @Bean
+    public MessageListenerAdapter endConfirmAdapter(EndVoteSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "sendMessage");
+    }
+
+    @Bean
+    public ChannelTopic endConfirmTopic() {
+        return new ChannelTopic("END_CONFIRM");
+    }
 }
