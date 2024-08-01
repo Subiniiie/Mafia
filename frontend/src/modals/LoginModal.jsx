b27341256674925cfb4073249b8c21a0bf8bf0f5
김송희 <kkbuh2@gmail.com>
2024-08-01 12:34:16 +0900

    [feat] searchBar CSS develop
    Former-commit-id: 263e7fd0690025c78488749e2adb28eff7cce0a8

--- frontend/src/modals/LoginModal.jsx
+++ frontend/src/modals/LoginModal.jsx
@@ -2,13 +2,30 @@ import ModalHeader from "../components/ModalHeader"
 import FindPwModal from "./FindPwModal";
 import styles from "./LoginModal.module.css"
 import { useState } from "react";
-
+import axios from "axios";
 
 const LoginModal = ({ isOpen, openModal }) => {
     const modalTitle = 'Login Modal';
 
     const [isFindPwModalOpen, setIsFindPwModalOpen] = useState(false)
     const openFindPwModal = () => setIsFindPwModalOpen
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('https://i11e106.p.ssafy.io/api/login', {
+                email: email,
+                password: password
+            });
+            console.log(response.data);
+            // 로그인 성공 시 처리 로직
+        } catch (error) {
+            console.error("Login failed:", error);
+            // 로그인 실패 시 처리 로직
+        }
+    };
 
     if (!isOpen) return null; // 모달이 열리지 않았다면 렌더링하지 않음
 
@@ -23,6 +40,8 @@ const LoginModal = ({ isOpen, openModal }) => {
                         type="text"
                         placeholder="이메일을 입력해주세요"
                         className={styles.inputField}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
 
                     <h5>비밀번호</h5>
@@ -31,9 +50,11 @@ const LoginModal = ({ isOpen, openModal }) => {
                         type="text"
                         placeholder="비밀번호를 입력해주세요"
                         className={styles.inputField}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
-                    <button className={styles.submitButton}>
+                    <button className={styles.submitButton} onClick={handleLogin}>
                         활동하기
                     </button>
 
