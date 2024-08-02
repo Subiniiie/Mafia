c288a0c0b0dff84ea3c59ea2e1b7e2cb5b577d35
박수빈 <tns027@naver.com>
2024-08-02 09:36:16 +0900

    [✨feat]  Game page 직업 카드 & 방이름 CSS
    Former-commit-id: 22466feef51f5831acbae009ee85a6357a7fb51b

--- frontend/src/components/GamePageComponents/GamePageHeader.jsx
+++ frontend/src/components/GamePageComponents/GamePageHeader.jsx
@@ -7,7 +7,7 @@ function GamePageHeader() {
     // 게임 리스트에서 게임 방을 클릭하면 or 방장이 새롭게 게임 방을 만들면 or 방장이 방 이름을 수정하면
     // 게임 방 정보가 나한테 들어올 거임
     // 그때 코드 수정하기 지금은 임시로 설정
-    const [ roomTitle, setRoomTitle ] = useState('방이름')
+    const [ roomTitle, setRoomTitle ] = useState('대한의 독립을 위하여')
 
     // 방장 정보는 어디서 얻음??
     // 백에서 주겠지
@@ -29,7 +29,9 @@ function GamePageHeader() {
         <>
             <div className={styles.container}>
                 <div className={styles.header}>
-                    {roomTitle}
+                    <div className={styles.roomTitle}>
+                        {roomTitle}
+                    </div>
                     <div className={styles.right}>
                         {roomManager ? roomManagerSettings : null}
                             <Link to="/game-list" className={styles.exit}>
