8cb3ef2602baa7df7d527d12bfd8e77e8642664e
박수빈 <tns027@naver.com>
2024-08-02 17:15:03 +0900

    [✨✨feat] 게임 페이지 닉네임 & 투표 버튼 & 음소거 버튼 생성, 게임 페이지  및 업적 페이지 서버 연결 코드 작성
    Former-commit-id: 16b9ddb1066e3e759eb42184e2dab746dc464e56

--- frontend/src/modals/JobModal.jsx
+++ frontend/src/modals/JobModal.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ModalHeader from "../components/ModalHeader";
 import styles from "./JobModal.module.css"
 
@@ -7,9 +7,13 @@ function JobModal({ isOpen, openModal }) {
     // 직업에 따라 모달의 내용이 달라짐
     // 플레이어의 직업 정보를 받아옴
     // 지금은 없으니까 임시로 설정
+    // 직업을 어떻게 받아올거야야야야야야야야야야야
+    // 독립운동가 -> 변절자 될 때 어떻게 데이터를 또 가져옴?
+    // 매번 서버에서 가지고 오는 거니까 가지고 올 경우는 고려X?
+    // 그럼 선택/투표를 서버로 어떻게 보내느냐 고려?
     const jobs = {
         '독립운동가': {
-            '설명': '독립운동가는 밀정과 변절자를 찾아내서 제거해야합니다.\n' +
+            '설명': ' 독립운동가는 밀정과 변절자를 찾아내서 제거해야합니다.\n' +
                     ' 투표로 밀정과 변절자를 찾아낼 수 있습니다. 다만 독립운동가는 밀정에 의해 변절될 수 있습니다.', 
             '이미지': '/jobs/독립운동가.png'
         },
@@ -30,28 +34,70 @@ function JobModal({ isOpen, openModal }) {
             '이미지': '/jobs/변절자.png'
         }
     }
-    const [ myJob, setMyJob ] = useState('첩보원')
 
-    function checkMyJob() {
-        setMyJob('변절자')
-    }
+    // 초기 상태 설정 
+    // 직업 정보 입력 받을 때 초기값 바꾸기
+    const [ myJob, setMyJob ] = useState('밀정')
+    const [ jobDetails, setJobDetails ] = useState(' 밀정은 독립운동가와 첩보원을 전부 제거해야합니다.\n' + ' 매일 밤 독립운동가를 살해할 수 있으며 딱 한 번, 독립운동가 한 명을 변절시킬 수 있습니다.')
+    const [ jobImage, setJobImage ] = useState('/jobs/밀정.png')
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState('')
 
 
-    const jobDetails = jobs[myJob]
+    // 서버 연결하면 쓸 코드
+    // // 서버에서 직업 정보를 받아오는 함수
+    // const fetchJobDetails = async function() {
+    //     setLoading(true)
+    //     setError('')
+    //     try {
+    //         // url을 뭐를 써야할까
+    //         const response = await fetch("정보를 받을 url")
+    //         if (!response.ok) {
+    //             throw new Error('Failed to fetch job defatuls')
+    //         }
+    //         // 플레이어 정보를 받음
+    //         const data = await response.json()
+    //         setMyJob(data.job)
+    //         setJobDetails(jobs[data.job]['설명'])
+    //         setJobImage(jobs[data.job]['이미지'])
+    //     } catch(error) {
+    //         setError(error.message)
+    //     } finally {
+    //         setLoading(false)
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     fetchJobDetails()
+    // }, [])
+
+    // if (loading) {
+    //     return <div>Loading...</div>
+    // }
+
+    // if (error) {
+    //     return <div>Error: {error}</div>
+    // }
+
+    // if (!jobDetails) {
+    //     return null
+    // }
+
+
 
     return (
         <>
             <div className={styles.modals}>
                 <div className={styles.content}>
                     <div className={styles.front}>
-                        <img src={jobDetails['이미지']} alt={myJob} />
+                        <img src={jobImage} alt={myJob} />
                     </div>
                     <div className={styles.back}>
                         <ModalHeader modalTitle="직업 설명" openModal={openModal} />
                         <div className={styles.backContent}>
                             <h3>당신은<span style={{ color:'rgba(139, 0, 0, 0.9)', fontWeight: 'bold'}}>{myJob}</span>입니다.</h3>
                             <br />
-                            <p>{jobDetails['설명']}</p>
+                            <p>{jobDetails}</p>
                         </div>
                     </div>
                 </div>
