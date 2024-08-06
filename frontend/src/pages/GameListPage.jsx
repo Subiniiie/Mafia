5642429016392a989ab2e5ad8d1e28248d87b443
김송희 <kkbuh2@gmail.com>
2024-08-06 15:28:51 +0900

    [fix] GLHeader 관련 CSS 파일 삭제 및 재작업 중
    Former-commit-id: ebef680a5a6d325231508e488f501753756617a4

--- frontend/src/pages/GameListPage.jsx
+++ frontend/src/pages/GameListPage.jsx
@@ -1,14 +1,12 @@
 import GLHeader from "../components/GameListComponents/GLHeader";
 import GLMain from "../components/GameListComponents/GLMain";
 import GLFooter from "../components/GameListComponents/GLFooter";
-import styles from "./GameListPage.module.css";
-
 
 function GameListPage() {
     return (
-        <div className={styles.layout}>
+        <div>
             <GLHeader />
-            <main className={styles.main}>
+            <main>
                 <GLMain />
             </main>
             <GLFooter />
