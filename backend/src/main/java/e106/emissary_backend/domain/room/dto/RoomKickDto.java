a015672346ec3872a57667db31d44e03cafffc19
김강진 <kkj1250@gmail.com>
2024-08-07 11:56:58 +0900

    [feat] done leave session, change role ( user -> host ), kick - not tested.
    Former-commit-id: 6f279a82997574bd587fc21e14a35b7b342ab011

---
+++ backend/src/main/java/e106/emissary_backend/domain/room/dto/RoomKickDto.java
@@ -0,0 +1,15 @@
+package e106.emissary_backend.domain.room.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomKickDto {
+    private Long roomId;
+    private String ConnectionId;
+}
