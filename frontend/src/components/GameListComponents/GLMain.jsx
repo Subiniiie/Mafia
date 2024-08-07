35b33541819bd2eda80f10ba4ffd7431155f8ac6
김송희 <kkbuh2@gmail.com>
2024-08-07 23:14:57 +0900

    [feat] GLMain API 연결 - 기본 코드 작업 중
    Former-commit-id: 5caa43ac4b32601b3504e5595f5710769ab05448

--- frontend/src/components/GameListComponents/GLMain.jsx
+++ frontend/src/components/GameListComponents/GLMain.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import GameRoomCard from "./GameRoomCard";
 import styles from "./GLMain.module.css";
 
 const GLMain = () => {
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+        const fetchRooms = async () => {
+            try {
+                const response = await axios.get('https://i11e106.p.ssafy.io/api/rooms')
+                console.log(response.data)
+                setRooms(response.data)
+            } catch (error) {
+                console.error("Failed to fetch rooms :", error)
+            }
+        }
+
+        fetchRooms()
+    })
+
+
     return (
         <div className={styles.container}>
+            {rooms.map((room) => (
+                <div className={styles.cardWrapper} key={room.id}>
+                    <GameRoomCard
+                        id={room.id}
+                        title={room.title}
+                        ownerName={room.ownerName}
+                        progress={room.progress}
+                        inInProgress={room.isInProgress}
+                    />
+                </div>
+            ))}
+
+            {/* 
+
             <div className={styles.cardWrapper}>
                 <GameRoomCard id="1" title="고국으로" leader="강진" progress={7} isInProgress={false} />
             </div>
@@ -19,46 +52,9 @@ const GLMain = () => {
             <div className={styles.cardWrapper}>
                 <GameRoomCard id="2" title="대한의 독립을 위하여" leader="수빈" progress={6} isInProgress={false} />
             </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="3" title="고향의 푸른 산하" leader="최진" progress={8} isInProgress={true} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="1" title="고국으로" leader="강진" progress={7} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="2" title="대한의 독립을 위하여" leader="수빈" progress={6} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="3" title="고향의 푸른 산하" leader="최진" progress={8} isInProgress={true} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="1" title="고국으로" leader="강진" progress={7} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="2" title="대한의 독립을 위하여" leader="수빈" progress={6} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="3" title="고향의 푸른 산하" leader="최진" progress={8} isInProgress={true} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="1" title="고국으로" leader="강진" progress={7} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="2" title="대한의 독립을 위하여" leader="수빈" progress={6} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="3" title="고향의 푸른 산하" leader="최진" progress={8} isInProgress={true} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="1" title="고국으로" leader="강진" progress={7} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="2" title="대한의 독립을 위하여" leader="수빈" progress={6} isInProgress={false} />
-            </div>
-            <div className={styles.cardWrapper}>
-                <GameRoomCard id="3" title="고향의 푸른 산하" leader="최진" progress={8} isInProgress={true} />
-            </div>
-            {/* 필요 시 더 많은 카드 추가 */}
+            
+
+            필요 시 더 많은 카드 추가 */}
         </div>
     );
 };
