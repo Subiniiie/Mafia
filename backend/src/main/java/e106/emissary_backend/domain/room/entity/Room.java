60bec8bd5416bf76d4066382d24048d20e60f2cb
이현규 <gusrb2776@naver.com>
2024-08-08 01:42:39 +0900

    [feat] add Game_UserInGame cascade option
    Former-commit-id: cb1ead60b143003c074dda4bbd444cb1fe3d9c4f

--- backend/src/main/java/e106/emissary_backend/domain/room/entity/Room.java
+++ backend/src/main/java/e106/emissary_backend/domain/room/entity/Room.java
@@ -2,6 +2,7 @@ package e106.emissary_backend.domain.room.entity;
 
 import e106.emissary_backend.domain.room.dto.RoomRequestDto;
 import e106.emissary_backend.domain.room.enumType.RoomState;
+import e106.emissary_backend.domain.userInRoom.entity.UserInRoom;
 import e106.emissary_backend.global.common.BaseTimeEntity;
 import io.jsonwebtoken.lang.Objects;
 import jakarta.persistence.*;
@@ -9,6 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -43,6 +47,9 @@ public class Room extends BaseTimeEntity {
 
     @Column(name = "have_betray")
     private boolean haveBetray;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<UserInRoom> userInRooms = new ArrayList<>();
 
     public void update(RoomRequestDto roomRequestDto) {
         title = roomRequestDto.getTitle();
