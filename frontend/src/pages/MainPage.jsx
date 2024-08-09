c5990f3a62f977b9c9038b7c5b9697424b734b86
김송희 <kkbuh2@gmail.com>
2024-08-09 22:03:03 +0900

    [feat]isLoggedIn 여부에 따른 GameListPage 이동 차단 및 MainPage CSS | 버튼 클릭 안됨 Error 발생
    Former-commit-id: 0a6be94f3c0ce4d5f90674196b12f8156513ef53

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import classNames from "classnames";
 import { Link } from "react-router-dom";
 import bgVideo from "../assets/video/video.mp4"
 import styles from './MainPage.module.css'
 
-function MainPage() {
+function MainPage({ isLoggedIn }) {
+    const videoContainerRef = useRef()
+    const videoRef = useRef()
+
+    const MainPageClass = classNames('east-sea-dokdo-regular', styles.content)
+
+    const alertMessage = () => {
+        alert('로그인을 해주세요.')
+    }
+
+    useEffect(() => {
+        const adjustVideoContainerHeight = () => {
+            if (videoRef.current && videoContainerRef.current) {
+                const videoAspectRatio = videoRef.current.videoWidth / videoRef.current.videoHeight;
+                const containerWidth = videoContainerRef.current.offsetWidth;
+                const containerHeight = containerWidth / videoAspectRatio;
+                videoContainerRef.current.style.height = `${containerHeight}px`;
+            }
+        }
+
+        // 비디오가 로드된 후 높이를 조정
+        videoRef.current.addEventListener('loadedmetadata', adjustVideoContainerHeight)
+
+        // 창 크기가 변경될 때도 높이를 재조정
+        window.addEventListener('resize', adjustVideoContainerHeight)
+
+        return () => {
+            window.removeEventListener('resize', adjustVideoContainerHeight)
+        }
+    }, [])
+
     return (
         <div className={styles.container}>
-            <div className={styles.vidioContainer}>
-                <video autoPlay loop muted className={styles.backgroundVideo}>
-                    <source src={bgVideo} type="video/mp4"/>
+            <div ref={videoContainerRef} className={styles.videoContainer}>
+                <video ref={videoRef} autoPlay loop muted className={styles.backgroundVideo}>
+                    <source src={bgVideo} type="video/mp4" />
                 </video>
-            </div>
-            <div className={styles.content}>
-                <Link to="/game-list">여정시작</Link>
+                <div className={MainPageClass}>
+                    {isLoggedIn ? (
+                        <Link to="/game-list" className={styles.workingButton}>여정 떠나기</Link>
+                    ) : (
+                        <div className={styles.blockedButton} onClick={alertMessage}>
+                            여정 떠나기
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
