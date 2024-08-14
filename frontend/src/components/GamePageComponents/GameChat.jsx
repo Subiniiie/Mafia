b326262ab9aa186dfa21a2e95d7564418679aacc
김강진 <kkj1250@gmail.com>
2024-08-14 17:38:20 +0900

    [fix] remove await @GameChat.jsx
    Former-commit-id: c7e8a6085e95836e91ce8b81617d5fea1aa708dd

--- frontend/src/components/GamePageComponents/GameChat.jsx
+++ frontend/src/components/GamePageComponents/GameChat.jsx
@@ -18,7 +18,7 @@ function GameChat({ systemMessage, session, chatMode, chatHistory, players }) {
     // chatMode.mode에 따라 모든 유저에게 전송할 지, 밀정/변절자에게만 전송할 지 결정
     // 밤일 경우, 밀정/변절자가 아닌 유저면 전송하면 안 됨 => 조건 분기 필요
     const handleChatSubmit = (message) => {
-        const valid = players.filter(player => player.isMe).map(player => await isEmissaryOrBetrayer(player))[0];
+        const valid = players.filter(player => player.isMe).map(player => isEmissaryOrBetrayer(player))[0];
         console.log("mode",chatMode.mode);
         if (chatMode.mode === 'signal:chat' || valid) {
             session.signal({
