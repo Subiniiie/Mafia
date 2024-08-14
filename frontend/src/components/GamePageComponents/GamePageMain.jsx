a6d18d1ee10896039814c36beeecde8603700402
김강진 <kkj1250@gmail.com>
2024-08-14 16:23:15 +0900

    [fix] brackets error
    Former-commit-id: 67d3a133d502825085c77e08a48c337d1a67f3a3

--- frontend/src/components/GamePageComponents/GamePageMain.jsx
+++ frontend/src/components/GamePageComponents/GamePageMain.jsx
@@ -278,7 +278,7 @@ function GamePageMain({ setSystemMessage, roomId, streamManagers, setChatMode, s
         // 밀정, 변절자를 제외한 유저는 비디오/오디오를 publish 하지도 않고,
         // 다른 유저들의 비디오/오디오를 subscribe 하지도 않는다.
         //if (!isEmissaryOrBetrayer(players[publisherIdx])) {
-        if (!isEmissaryOrBetrayer())
+        if (!isEmissaryOrBetrayer()){
             streamManagers[publisherIdx].publishVideo(false);
             streamManagers[publisherIdx].publishAudio(false);
 
@@ -573,7 +573,6 @@ function GamePageMain({ setSystemMessage, roomId, streamManagers, setChatMode, s
             </div> : null}
       </>
     )
-    }
 }
 
 export default GamePageMain;
