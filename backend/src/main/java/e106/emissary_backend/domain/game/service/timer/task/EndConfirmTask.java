d915f58e5432ba8ef65969db0a4031c95493b1d8
이현규 <gusrb2776@naver.com>
2024-08-15 14:21:48 +0900

    [fix] endconfirm 레디스에 저장로직 수정
    Former-commit-id: e135911e0a089b5d4e24c0c084f4dff685218b75

--- backend/src/main/java/e106/emissary_backend/domain/game/service/timer/task/EndConfirmTask.java
+++ backend/src/main/java/e106/emissary_backend/domain/game/service/timer/task/EndConfirmTask.java
@@ -65,7 +65,11 @@ public class EndConfirmTask implements GameTask {
 //        redisKeyValueTemplate.update(gameDTO.toDao());
 
         String voteKey = GameConstant.VOTE_KEY_PREFIX + gameId;
-        HashMap<Long, Integer> voteMap = voteRedisTemplate.opsForValue().get(voteKey);
+//        HashMap<Long, Integer> voteMap = voteRedisTemplate.opsForValue().get(voteKey);
+        HashMap<Long, Integer> voteMap = getVoteMapFromRedis(voteKey);
+        log.info("들고온 voteMap: {}",voteMap.toString());
+
+
         if (voteMap == null) {
             voteMap = new HashMap<>();
         }
@@ -106,6 +110,23 @@ public class EndConfirmTask implements GameTask {
         }
 //        gameService.endConfirm(EndConfirmMessage.builder().gameId(gameId).build());
     }
+
+    @RedissonLock(value = "#gameId")
+    private HashMap<Long, Integer> getVoteMapFromRedis(String voteKey) {
+        HashMap<Long, Integer> result = voteRedisTemplate.opsForValue().get(voteKey);
+        if (result == null) {
+            return new HashMap<>();
+        }
+
+        if (result instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) result;
+            return map.entrySet().stream()
+                    .collect(HashMap::new,
+                            (m, e) -> m.put(Long.valueOf(e.getKey().toString()), (Integer) e.getValue()),
+                            HashMap::putAll);
+        }
+        return new HashMap<>();
+    } // end of getVoteMapFromRedis
 
     public void setGameId(long gameId){
         this.gameId = gameId;
