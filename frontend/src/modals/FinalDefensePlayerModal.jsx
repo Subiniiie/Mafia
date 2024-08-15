7e3b40ff86d5c3a43f6b5089d1f127911718bb79
김종훈 <boll0918@naver.com>
2024-08-15 21:45:26 +0900

    [fix] 게임 로직 전반적인 수정
    Former-commit-id: fe06caccfa7ded6a4d997d56b144abbe9c6256db

--- frontend/src/modals/FinalDefensePlayerModal.jsx
+++ frontend/src/modals/FinalDefensePlayerModal.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
+import styles from "./FinalDefensePlayerModal.module.css"
+import { Client } from '@stomp/stompjs';
 
-const FinalDefensePlayer = function({ suspect, onMessage }) {
+const FinalDefensePlayerModal = function({ suspect, onMessage, roomId, stompClient, setFinalDefensePlayer }) {
+
+    const access = localStorage.getItem('access');
+    const header =  {'Authorization': `Bearer ${access}`}
 
     const choiceDie = () => {
         onMessage = "찬성"
+
+        // body에다가 JSON으로
+        // confirm: true
+        setFinalDefensePlayer(false)
+
+        stompClient.current.send(
+            `/ws/pub/confirm/${roomId}`, 
+            header,
+            JSON.stringify({
+                confirm: true
+            })
+        )
     }
+
+
     const choiceAlive = () => {
         onMessage = "반대"
+
+        setFinalDefensePlayer(false)
+
+        stompClient.current.send(
+            `/ws/pub/confirm/${roomId}`, 
+            header,
+            JSON.stringify({
+                confirm: false
+            })
+        )
     }
 
     return (
@@ -24,4 +53,4 @@ const FinalDefensePlayer = function({ suspect, onMessage }) {
     )
 }
 
-export default FinalDefensePlayer;
+export default FinalDefensePlayerModal;
